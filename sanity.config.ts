7af7f94359211398a917756d84ed3f8706987871
ChrisKnowsDev <chrisharley81@gmail.com>
2026-07-29 14:48:00 -0400

    fix(sanity): add basePath to Studio config for correct routing

--- sanity.config.ts
+++ sanity.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   title: 'BASSMENT CMS',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'cp66glrr',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  basePath: '/studio',
   plugins: [
     structureTool({ structure }),
     visionTool(),
